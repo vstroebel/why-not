faa2e3a90e7cb03830d04131416ef7b056347a75
Volker Ströbel <volkerstroebel@mysurdity.de>
2021-10-03 09:29:39 +0200

    Support printing to stderr

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
+#![allow(clippy::collapsible_else_if)]
+
 use std::env;
 use clap::{App, Arg, ArgMatches, crate_name, crate_version, crate_description, value_t, ErrorKind};
+use std::io::Write;
 
 const BUFF_SIZE: usize = 8192;
 
+const STD_ERR_ARG: &str = "STD_ERR";
 const MAX_ARG: &str = "MAX";
 const STRING_ARG: &str = "STRING";
 
@@ -22,16 +26,33 @@ pub fn main() {
         },
     };
 
+    let stderr = matches.is_present(STD_ERR_ARG);
+
     if let Some(max) = max {
-        for _ in 0..max {
-            println!("{}", message)
+        if stderr {
+            print_max(std::io::stderr(), &message, max);
+        } else {
+            print_max(std::io::stdout(), &message, max);
         }
     } else {
-        let buffer = get_buffer(&message);
-
-        loop {
-            print!("{}", buffer)
+        if stderr {
+            print_infinitely(std::io::stderr(), &message);
+        } else {
+            print_infinitely(std::io::stdout(), &message);
         }
+    }
+}
+
+fn print_max<W: Write>(mut w: W, message: &str, max: usize) {
+    for _ in 0..max {
+        writeln!(w, "{}", message);
+    }
+}
+
+fn print_infinitely<W: Write>(mut w: W, message: &str) {
+    let buffer = get_buffer(message);
+    loop {
+        w.write_all(buffer.as_bytes());
     }
 }
 
@@ -68,6 +89,10 @@ fn create_clap_app() -> App<'static, 'static> {
     App::new(crate_name!())
         .version(crate_version!())
         .about(crate_description!())
+        .arg(Arg::with_name(STD_ERR_ARG)
+            .short("e")
+            .long("stderr")
+            .help("Print to stderr"))
         .arg(Arg::with_name(MAX_ARG)
             .short("m")
             .long("max")
